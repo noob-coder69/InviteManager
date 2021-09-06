d8cb67ed741c446e536f20e62d6c73bdf142742f
noob-coder69 <78252850+noob-coder69@users.noreply.github.com>
2021-09-06 12:28:55 +0530

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -84,7 +84,7 @@ module.exports = async (client, member) => {
             if (userData.invites >= nbInv) {
                 let inviterMember = member.guild.member(usedInvite.inviter.id)
                 if (!inviterMember) return;
-                if (!inviterMember.roles.cache.find(r => r.id === roleID)) {
+                else if (!inviterMember.roles.cache.find(r => r.id === roleID)) {
                     inviterMember.roles.add(member.guild.roles.cache.find(r => r.id === roleID))
                 }
             }
